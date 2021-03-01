cabc4616febc373202729db357703699a0207bf6
AlastairSmith15 <alastair.smith1997@gmail.com>
2021-03-01 18:24:52 +0000

    Loop example

--- cluedoproject/src/com/codewithAlastair/simpleforloop.java
+++ cluedoproject/src/com/codewithAlastair/simpleforloop.java
@@ -3,8 +3,7 @@ package com.codewithAlastair;
 public class simpleforloop {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++)
-            System.out.println("Hello World");
-
+        for (int i = 5; i > 0; i--)
+            System.out.println("Hello World " + i);
     }
 }
