421d1588c2406b5bd36f245c95e21861f3d8ea28
AlastairSmith15 <alastair.smith1997@gmail.com>
2021-03-01 17:06:54 +0000

    Adding Fizz Buzz

---
+++ cluedoproject/src/com/codewithAlastair/whileAndDoWhileLoops.java
@@ -0,0 +1,22 @@
+package com.codewithAlastair;
+
+import java.util.Scanner;
+
+public class whileAndDoWhileLoops {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input = "";
+        while (!input.equals("quit")) {
+            System.out.print("Input:");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        }
+
+        do {
+            System.out.print("Input: ");
+            input = scanner.next().toLowerCase();
+            System.out.println(input);
+        } while (!input.equals("quit"));
+    }
+}
