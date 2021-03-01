f782f3bda12918108b3676ba3ccc120a2f2f8777
AlastairSmith15 <alastair.smith1997@gmail.com>
2021-03-01 18:24:36 +0000

    Learning java updated

--- cluedoproject/src/com/codewithAlastair/simplestructure.java
+++ cluedoproject/src/com/codewithAlastair/simplestructure.java
@@ -1,8 +1,19 @@
 package com.codewithAlastair;
 
+
+import java.util.Scanner;
+
 public class simplestructure {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String[] fruits = { "Apple", "Mango", "Orange"};
+
+        for (int i = 2; i >= 0; i--)
+            System.out.println(fruits[i]);
+
+        for (String fruit : fruits)
+            System.out.println(fruit);
     }
 }
+
+
